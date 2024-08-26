4240adc62558a6cfbea24b9743ac216977f46c9b
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-25 23:03:27 -0300

    Create index1.js

---
+++ 008-selecionando elementos/index1.js
@@ -0,0 +1,7 @@
+"use strict"
+const h1 = document.querySelector("h1"); //Acessando por TAG
+const Classp = document.querySelector(".paragrafo");
+const idP = document.querySelector("#paragrafo"); //Acessando o id dentro da TAG
+console.log(h1);
+console.log(Classp.querySelector("strong"));  //Acessando o strong de dentro do p dentra da TAG
+console.log(idP);
