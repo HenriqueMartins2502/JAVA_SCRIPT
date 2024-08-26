0d61e444631f37dbe38418133baa72a264c28258
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-25 22:53:27 -0300

    Create index.js

---
+++ 006-objetos/index.js
@@ -0,0 +1,22 @@
+// O que é um objeto
+
+let Tenis = {
+    tipo: "Tenis de corrida",
+    cardaco: "G",
+    estoque: true,
+    tamanho: {
+        palmilha: 45,
+        tenis: 42,
+        caixa: {
+            tamanho: 56,
+            altura: 78,
+            profundidade: 888,
+        },
+    },
+    marca:["nike", "adidas", "puma", "new balance"],
+};
+
+console.log(Tenis.tipo);
+console.log(Tenis.tamanho);
+console.log(Tenis.estoque);
+console.log(Tenis.marca);
