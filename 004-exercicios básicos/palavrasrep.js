e7aeb92441fba7ed1b75538c1890219e56151ce0
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-25 22:50:49 -0300

    Create palavrasrep.js

---
+++ 004-exercicios básicos/palavrasrep.js
@@ -0,0 +1,11 @@
+const palavra = 'Abacate'.toLocaleLowerCase();
+let letras = {};
+
+for( let i = 0;i < palavra.length; i++){
+    if (letras[palavra[i]]){
+        letras[palavra[i]]++;
+    }else{
+        letras[palavra[i]] = 1;
+    }
+}
+console.log(letras);
