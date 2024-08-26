7e7dc8af42e8747b5c45a3f84e4680dd831a6d21
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-25 22:49:42 -0300

    Create index.js

---
+++ 003-operador ternario/index.js
@@ -0,0 +1,9 @@
+// Operador ternario
+
+let velocidade = 80;
+ let warn = 75;
+
+const condicao = 
+    velocidade > warn 
+        ? console.log("MULTA!!!")
+        : console.log("Velocidade normal");
